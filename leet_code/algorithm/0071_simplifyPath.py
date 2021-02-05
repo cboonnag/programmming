30691e8e62c2684d5a8cc6653e3af809336d5a62
cboonnag <c.boonnag@gmail.com>
2021-02-06 00:19:34 +0700

    revise 0071

--- leet_code/algorithm/0071_simplifyPath.py
+++ leet_code/algorithm/0071_simplifyPath.py
@@ -1,32 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        ls = path.split("/")
-
-        new_ls = ["" if x=="." else x for x in ls]
-        new_path = [x for x in new_ls if (x != "")]
-
-        n = len(new_path)
-        i = 0
-
-        while i < n:
-            if (i < n-1) and (new_path[i+1] == ".."):
-                new_path[i] = ""
-                new_path[i+1] = ""
-            if(new_path[i] == ".."):
-                new_path[i] = ""
-            
-            i += 1
-            
-            if ("" in new_path):
-
-                new_path = [value for value in new_path if value != ""]
-
-                i = 0
-                n = len(new_path)
-            
-
-        new_path = [x for x in new_path if (x != "")]    
-        separater = "/"
-        a = separater.join(new_path)
-
-        return "/"+a
+        dir_list = [x for x in path.split("/") if x]
+        result = list()
+        for path in dir_list:
+            if path =="..":
+                if result:
+                    result.pop()
+            elif path ==".":
+                pass
+            else:
+                result.append(path)
+        return "/"+"/".join(result) 
